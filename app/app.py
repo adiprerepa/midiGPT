6895982c919eb36433f248407fad7061424a85cd
tarsur909 <68882529+tarsur909@users.noreply.github.com>
2024-05-01 23:25:41 -0500

    final fixes

--- app/app.py
+++ app/app.py
@@ -3,7 +3,8 @@ import json
 import torch
 from midigpt import GPT, TetradPlayer, WatermarkLogitsProcessor, WatermarkDetector
 from midigpt.datasets import BachChoralesEncoder
-
+# import pathlib
+import os
 app = Flask(__name__)
 
 default_metrics = [
@@ -19,7 +20,7 @@ default_metrics = [
 @app.route('/')
 def index():
     metrics = default_metrics
-    return render_template('index.html', metrics=metrics, w_metrics=metrics, detect_metrics=metrics)
+    return render_template('index.html', metrics=metrics, w_metrics=metrics, detect_metrics=metrics, unwatermarked_generation="/Users/tarsur909/Documents/GitHub/midiGPT/app/out/unwatermarked_generation.wav")
 
 @app.route('/generate_music', methods=['POST'])
 def generate_music():
@@ -31,7 +32,7 @@ def generate_music():
     # write file to temp
     with open("/tmp/temp.wav", 'wb') as f:
         f.write(music_file.read())
-    music_file = "/Users/ayushraman/Documents/GitHub/midiGPT/generated-bach-1.5.wav"
+    music_file = "/tmp/temp.wav"
     
     #load model and tokenizer
     model = GPT.from_checkpoint("../projects/bach-chorales/best_model.ckpt", map_location=torch.device("cpu"))
@@ -41,6 +42,7 @@ def generate_music():
     tempo = int(request.form['tempo'])
     do_sample = True
     temperature= float(request.form['temperature'])
+    print(temperature)
 
     print(f"max_tokens: {max_tokens}, tempo: {tempo}, do_sample: {do_sample}, temperature: {temperature}")
     
@@ -66,18 +68,27 @@ def generate_music():
 
     watermarked_detection_result = watermark_detector.detect(text= watermarked_chords[-(max_tokens):])
     
-    
-    combined_response = {
-        'unwatermarked_buffer': unwatermark_path,
-        'watermarked_buffer': watermark_path,
-        'unwatermarked_detection_result': unwatermarked_detection_result,
-        'watermarked_detection_result': watermarked_detection_result
-    }
-
+    #{'num_tokens_scored': 79, 'num_green_tokens': 24, 'green_fraction': 0.3037974683544304, 'z_score': 1.1042686641847725, 'p_value': 0.13473830583737795, 'prediction': False}
     try:
-        metrics = combined_response['unwatermarked_detection_result']
-        w_metrics = combined_response['watermarked_detection_result']
-        return render_template('index.html', metrics=metrics, w_metrics=w_metrics, detect_metrics=metrics)
+        metrics = [
+            {"name": "z-score Threshold", "value": 3.0},
+            {"name": "z-score", "value": unwatermarked_detection_result['z_score']},
+            {"name": "p value", "value": unwatermarked_detection_result['p_value']},
+            {"name": "Tokens Counted (T)", "value": unwatermarked_detection_result['num_tokens_scored']},
+            {"name": "Prediction", "value": unwatermarked_detection_result['prediction']},
+            {"name": "Fraction of T in Greenlist", "value": unwatermarked_detection_result['green_fraction']},
+            {"name": "# Tokens in Greenlist", "value": unwatermarked_detection_result['num_green_tokens']}
+        ]
+        w_metrics= [
+            {"name": "z-score Threshold", "value": 3.0},
+            {"name": "z-score", "value": watermarked_detection_result['z_score']},
+            {"name": "p value", "value": watermarked_detection_result['p_value']},
+            {"name": "Tokens Counted (T)", "value": watermarked_detection_result['num_tokens_scored']},
+            {"name": "Prediction", "value": watermarked_detection_result['prediction']},
+            {"name": "Fraction of T in Greenlist", "value": watermarked_detection_result['green_fraction']},
+            {"name": "# Tokens in Greenlist", "value": watermarked_detection_result['num_green_tokens']}
+        ]
+        return render_template('index.html', metrics=metrics, w_metrics=w_metrics, detect_metrics=default_metrics)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -88,26 +99,41 @@ def detect_watermark():
 
     music_file = request.files['music_file']
     
-    if music_file.filename == '':
-        return jsonify({'error': 'No file selected'}), 400
+    fpath = "/tmp/temp.wav"
+    if os.path.exists(fpath):
+        os.remove(fpath)
+    
+    with open("/tmp/temp.wav", 'wb') as f:
+        f.write(music_file.read())
+    music_file = "/tmp/temp.wav"
     
 
     encoder = BachChoralesEncoder()
     
-    tempo = int(request.args['tempo'])
+    # tempo = int(request.args['tempo'])
+    tempo = 220
     
     chorale = TetradPlayer().from_wav(music_file, tempo = tempo)
+    print(chorale)
     
     watermark_detector = WatermarkDetector(device = 'cpu', tokenizer= encoder)
     
     detection_result = watermark_detector.detect(text= torch.as_tensor(chorale.flatten()))
+    print(detection_result)
     
-    response = {'detection_result': detection_result}
 
     # Call the watermark detection API
     try:
-        metrics = response['detection_result']
-        return render_template('index.html', detect_metrics=metrics)
+        detect_metrics = [
+            {"name": "z-score Threshold", "value": 3.0},
+            {"name": "z-score", "value": detection_result['z_score']},
+            {"name": "p value", "value": detection_result['p_value']},
+            {"name": "Tokens Counted (T)", "value": detection_result['num_tokens_scored']},
+            {"name": "Prediction", "value": detection_result['prediction']},
+            {"name": "Fraction of T in Greenlist", "value": detection_result['green_fraction']},
+            {"name": "# Tokens in Greenlist", "value": detection_result['num_green_tokens']}
+        ]
+        return render_template('results.html', detect_metrics=detect_metrics)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
